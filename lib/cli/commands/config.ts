94b3f332642a9b43f5d039db0019206de4ec429a
kylqin <kylqin@outlook.com>
2021-11-28 18:14:23 +0800

    feat: cli command

--- lib/cli/commands/config.ts
+++ lib/cli/commands/config.ts
@@ -1,9 +1,25 @@
 import {green} from "chalk";
-import {readConfRaw} from "@/utils/conf";
+import {confFilePath, readConfRaw} from "@/utils/conf";
 import {L} from "@/utils/log";
+import execa from "execa";
 
-export async function config(command: string) {
-  const cnf = await readConfRaw(command);
+interface CliConfCmdOptions {
+  edit: boolean,
+  path: boolean
+}
 
-  L(green(JSON.stringify(cnf, null, 2)));
+export async function config(command: string, options: CliConfCmdOptions) {
+  if (options.path) {
+    const cfp = confFilePath(command);
+    L(green(cfp));
+  } else if (options.edit) {
+    const cfp = confFilePath(command);
+    const rr = await execa('code', [cfp]);
+
+    console.log(rr);
+  } else {
+    const cnf = await readConfRaw(command);
+
+    L(green(JSON.stringify(cnf, null, 2)));
+  }
 }
--- lib/cli/index.ts
+++ lib/cli/index.ts
@@ -10,7 +10,9 @@ program
 
 program
   .command('conf <command>')
-  .description('查看/编辑 cli 命令的配置信息')
+  .description('查看 cli 命令的配置信息')
+  .option('-p, --path', '显示 cli 命令的配置文件路径')
+  .option('-e, --edit', '编辑 cli 命令的配置信息')
   .action(config);
 
 program.parse();
